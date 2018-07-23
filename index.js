1403e8896449b9a06b52750f9b047b0c0115608b
Christine Patton <christine@pattoncreative.com>
2018-07-23 22:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var scuberGreetingForFeet;
 function scuberGreetingForFeet(someValue) {
   if (someValue<= 400) {
   return "This one is on me!";
-} else if (someValue>2000),(someValue<=2500) {
+} else if (someValue>2000), (someValue<=2500) {
   return "I will gladly take your thirty bucks.";
 } else if (someValue>2500) {
    return "No can do.";
