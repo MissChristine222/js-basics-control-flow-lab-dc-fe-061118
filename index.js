27f18a3694e75578bbbdb9ec271445cb37334003
Christine Patton <christine@pattoncreative.com>
2018-07-23 23:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function scuberGreetingForFeet(distance) {
 }
 
 function ternaryCheckCity (city) {
- const ternaryCheckCity=($city===‘NYC’) ? “Ok, sounds good.” : “No go.”
+ if (city="NYC") ? “Ok, sounds good.” : “No go.”;
 }
