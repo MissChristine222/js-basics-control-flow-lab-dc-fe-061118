0331bd872e2f74e6b258eefdaacc6020cb77d2c2
Christine Patton <christine@pattoncreative.com>
2018-07-23 23:47:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,7 @@ function scuberGreetingForFeet(distance) {
 function ternaryCheckCity (city) {
   return (city==='NYC') ? 'Ok, sounds good.' : 'No go.';
 }
+
+function switchOnCharmFromTip (amount) {
+  
+}
